f8d4905784365ca3979eaf86321ed3df0d84e495
CarlosNegrao <cnegrao2@gmail.com>
2024-03-06 19:40:06 -0300

    dev

--- APP/screens/home.py
+++ APP/screens/home.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import data_entry
+import reports
 
 def main():
     st.title("Bem-vindo à minha aplicação")
@@ -8,14 +10,26 @@ def main():
         Você pode encontrar várias funcionalidades aqui.
     """)
 
-    # Adicione links ou botões para outras partes da aplicação
+    # Links para outras partes da aplicação
     if st.button("Ir para a tela de entrada de dados"):
         # Redirecionar para a tela de entrada de dados
         st.write("Redirecionando para a tela de entrada de dados...")
+        redirect_to_data_entry()
 
     if st.button("Ir para a tela de consulta analítica"):
         # Redirecionar para a tela de consulta analítica
         st.write("Redirecionando para a tela de consulta analítica...")
+        redirect_to_reports()
+
+def redirect_to_data_entry():
+    # Redirecionar para a página de entrada de dados (data_entry.py)
+    # Você pode chamar a função que renderiza a página de entrada de dados aqui
+    data_entry.main()
+
+def redirect_to_reports():
+    # Redirecionar para a página de consulta analítica (reports.py)
+    # Você pode chamar a função que renderiza a página de consulta analítica aqui
+    reports.main()
 
 if __name__ == "__main__":
     main()
