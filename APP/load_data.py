a79d8c125bd7bf8bf2698e32ad0e7646c351f5a8
CarlosNegrao <cnegrao2@gmail.com>
2024-03-08 10:52:44 -0300

    dev telas ok

--- APP/load_data.py
+++ APP/load_data.py
@@ -1,15 +1,1 @@
-import streamlit as st
 
-def load_data_page():
-    st.title('Load Data')
-    
-    # Campos para entrada de dados
-    year = st.number_input("Year", min_value=1900, max_value=2100)
-    month = st.number_input("Month", min_value=1, max_value=12)
-    indicator = st.text_input("Indicator")
-    target = st.number_input("Target")
-
-    # Botão para enviar dados
-    if st.button("Submit"):
-        # Lógica para armazenar os dados no banco de dados
-        st.success("Data submitted successfully!")
