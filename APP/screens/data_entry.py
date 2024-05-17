ab5f6c0bd25544a1eb42fbd86d86aa11d1789d63
CarlosNegrao <cnegrao2@gmail.com>
2024-05-16 21:48:40 -0300

    entrada de dados, key dup

--- APP/screens/data_entry.py
+++ APP/screens/data_entry.py
@@ -3,6 +3,8 @@ import streamlit as st
 import duckdb
 
 # Função para obter a conexão com o banco de dados
+
+
 def get_db_connection():
     # Determina o caminho do banco de dados com base no ambiente
     print("Diretório atual de trabalho:", os.getcwd())
@@ -14,10 +16,11 @@ def get_db_connection():
         db_path = '/mount/src/-kpy_analytics/APP/data/kpi_analytics_db.duckdb'
     else:
         # Caminho local para desenvolvimento
-        db_path = os.path.join(os.getcwd(), 'APP', 'data', 'kpi_analytics_db.duckdb')
+        db_path = os.path.join(os.getcwd(), 'APP', 'data',
+                               'kpi_analytics_db.duckdb')
 
     print(f"Connecting to database at: {db_path}")
-    
+
     try:
         # Tenta conectar ao banco de dados e retorna a conexão
         connection = duckdb.connect(db_path)
@@ -36,19 +39,29 @@ def load_indicators():
     return [(indicator[0], indicator[1]) for indicator in indicators]
 
 # Função para salvar os dados inseridos
+
+
 def save_data(year, month, selected_indicator_id, goal, value):
     try:
         with get_db_connection() as conn:
+            # Verifica se já existe um registro para este indicador no mês e ano especificados
+            existing = conn.execute("SELECT * FROM tb_monthly_data WHERE kpi_id = ? AND year = ? AND month = ?",
+                                    (selected_indicator_id, year, month)).fetchall()
+            if existing:
+                st.error(
+                    "Erro: Já existe registro para o indicador selecionado no período especificado. Por favor, verifique e tente novamente.")
+                return
+
+            # Insere os novos dados se não houver duplicidade
             conn.execute("INSERT INTO tb_monthly_data (kpi_id, year, month, goal, value) VALUES (?, ?, ?, ?, ?)",
                          (selected_indicator_id, year, month, goal, value))
             st.success("Dados enviados com sucesso! ✅")
     except duckdb.OperationalError as e:
-        if "unique constraint" in str(e).lower():
-            st.error("Erro: Já existe registro para o indicador selecionado no período especificado. Por favor, verifique e tente novamente.")
-        else:
-            st.error("Erro técnico ao enviar os dados. Por favor, tente novamente ou contate o suporte.")
+        st.error(
+            "Erro técnico ao enviar os dados. Por favor, tente novamente ou contate o suporte.")
     except Exception as e:
         st.error(f"Erro ao enviar os dados: {e}")
+
 
 # Função principal que roda a aplicação
 def main():
@@ -57,27 +70,33 @@ def main():
     with st.container():
         st.subheader("Selecione o Indicador 🔍")
         indicators = load_indicators()
-        selected_indicator_id, selected_indicator_name = st.selectbox('Escolha um indicador:', indicators, format_func=lambda x: x[1])
+        selected_indicator_id, selected_indicator_name = st.selectbox(
+            'Escolha um indicador:', indicators, format_func=lambda x: x[1])
 
     with st.container():
         st.subheader("Informe o Período 📅")
         col1, col2 = st.columns(2)
         with col1:
-            year = st.number_input("Ano", min_value=2024, max_value=2025, value=2024, help="Selecione o ano desejado.")
+            year = st.number_input(
+                "Ano", min_value=2024, max_value=2025, value=2024, help="Selecione o ano desejado.")
         with col2:
-            month = st.selectbox("Mês", range(1, 13), index=0, help="Selecione o mês desejado.")
+            month = st.selectbox("Mês", range(
+                1, 13), index=0, help="Selecione o mês desejado.")
 
     with st.container():
         st.subheader("Defina Valor e Meta 💹")
         col3, col4 = st.columns(2)
         with col3:
-            value = st.number_input("Valor", min_value=0.01, value=0.01, step=0.01, help="Insira o valor alcançado.")
+            value = st.number_input(
+                "Valor", min_value=0.01, value=0.01, step=0.01, help="Insira o valor alcançado.")
         with col4:
-            goal = st.number_input("Meta", min_value=0.01, value=0.01, step=0.01, help="Insira a meta estabelecida.")
+            goal = st.number_input(
+                "Meta", min_value=0.01, value=0.01, step=0.01, help="Insira a meta estabelecida.")
 
     submit_button = st.button("Enviar Dados 🚀")
     if submit_button:
         save_data(year, month, selected_indicator_id, goal, value)
+
 
 if __name__ == "__main__":
     main()
